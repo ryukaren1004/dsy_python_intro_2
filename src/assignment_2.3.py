be64e544183f241fd76117d22a0001280bd4e671
Karen Ryu <karenryu@Karens-MacBook.local>
2019-02-18 22:10:02 -0600

    Karen Ryu

--- src/assignment_2.3.py
+++ src/assignment_2.3.py
@@ -1,8 +1,24 @@
-# This first 6 lines are provided for you
 
-def computepay(h,r):
-    return 42.37
 
-hrs = input("Enter Hours:")
-p = computepay(10,20)
-print("Pay",p)
+def compute_total_average(eng, mat, stat):
+    if stat == 'total':
+        res = eng + math
+    elif stat == 'average':
+        res = (eng+math)/2
+    else:
+        res = None
+
+    return res
+
+english = input("Enter English score: ")
+english = int(english)
+
+# Write your code here
+math = input("Enter Math score: ")
+math = int(math)
+
+total = compute_total_average(english, math, 'total')
+average = compute_total_average(english, math, 'average')
+
+print('total =',total)
+print('average =',average)
